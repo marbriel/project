ca9e2190751050bfecb50d983a5c9d2dd6e5b247
marbiel <saintmarkhosena@gmail.com>
2022-03-28 16:13:24 +0800

    Update Transaction model and add relation to Specific Book And Borrower.

--- src/main/java/com/library/models/Transaction.java
+++ src/main/java/com/library/models/Transaction.java
@@ -3,6 +3,7 @@ package com.library.models;
 import com.library.utils.CalendarUtils;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.Date;
@@ -10,6 +11,7 @@ import java.util.Date;
 @Entity
 @Table(name = "transaction")
 @Getter
+@NoArgsConstructor
 public class Transaction {
     @Id
     @GeneratedValue(generator = "uuid")
@@ -17,8 +19,34 @@ public class Transaction {
     @Column(name = "id", nullable = false)
     private String id;
 
+
+
     private Date dateBorrowed;
     private Date dateExpectedReturn;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH}, orphanRemoval = true)
+    @JoinColumn(name = "specific_book_id")
+    private SpecificBook specificBook;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "borrower_id")
+    private Borrower borrower;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean isTransactionDone;
+
+    public Transaction(SpecificBook specificBook, Borrower borrower) {
+        this.specificBook = specificBook;
+        this.borrower = borrower;
+    }
+
+    public void setBorrower(Borrower borrower) {
+        this.borrower = borrower;
+    }
+
+    public void setSpecificBook(SpecificBook specificBook) {
+        this.specificBook = specificBook;
+    }
 
     public void setDateBorrowed(){
         this.dateBorrowed = CalendarUtils.getCurrentDate();
@@ -31,6 +59,5 @@ public class Transaction {
     public void setDateExpectedReturn(int days){
         this.dateExpectedReturn = CalendarUtils.expectedDateReturn(days);
     }
-
 
 }
